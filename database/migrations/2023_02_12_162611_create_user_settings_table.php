261b8cf132f276f34822ae429db3bad74eddb8dd
Reece Mathieson <swooperior@gmail.com>
2023-02-12 16:43:07 +0000

    Added UsesUuid trait to user models

---
+++ database/migrations/2023_02_12_162611_create_user_settings_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_settings', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->timestamps();
+            $table->uuid('user_id');
+            $table->string('setting');
+            $table->string('value')->nullable();
+
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_settings');
+    }
+};
