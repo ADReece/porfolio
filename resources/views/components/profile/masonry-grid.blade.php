bd9afd12de610ae14f02bd1830caed54ed8cf4ba
Reece Mathieson <swooperior@gmail.com>
2023-02-15 14:44:01 +0000

    Mobile cols diff from desktop

--- resources/views/components/profile/masonry-grid.blade.php
+++ resources/views/components/profile/masonry-grid.blade.php
@@ -126,6 +126,12 @@
     </div>
 </div>
 @php($cols = 4);
+@php($mobile_cols = 2)
+
+@if(preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]))
+    @php($$cols = $mobile_cols)
+@endif
+
 @php($size = (100 / $cols) - 1)
 
 <style>
