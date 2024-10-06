e567d98ff0f7506b14e28dd4ff5a48a307355882
Reece Mathieson <r.mathieson@oneandall.co.uk>
2024-10-06 18:02:41 +0100

    fix: Profile edit username, displaying correct users media

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -18,7 +18,7 @@ class ProfileUpdateRequest extends FormRequest
         return [
             'name' => ['string', 'max:255'],
             'email' => ['email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
-            'bio' => ['string', 'max:255']
+            'bio' => ['sometimes', 'nullable', 'string', 'max:255']
         ];
     }
 }
