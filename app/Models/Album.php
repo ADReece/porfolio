54cac3493c586c3d842efb02e12415066989f824
Reece Mathieson <r.mathieson@oneandall.co.uk>
2024-10-06 18:31:43 +0100

    fix: Added properties to album fillable, added cover photo to album

--- app/Models/Album.php
+++ app/Models/Album.php
@@ -7,10 +7,18 @@ use App\Models\Traits\UsesUuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Album extends Model
 {
     use HasFactory, Usesuuid, ScopesPublic;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'public',
+        'password'
+    ];
 
     /**
      * Get all the media for the Album
@@ -20,6 +28,14 @@ class Album extends Model
     public function media(): BelongsToMany
     {
         return $this->belongsToMany(Media::class);
+    }
+
+    public function cover(): HasOne
+    {
+        return $this->hasOne(Media::class, 'id', 'cover_id')
+            ->withDefault(function($model){
+                return $model->media()->take(1);
+            });
     }
 
 }
